d98b51d4d2d509db289379945a81350879c0aa7d
Trevor Fehrman <trevorfehrman@gmail.com>
2021-01-25 00:57:33 -0800

    literally nothing

--- src/screens/session-browser.screen.tsx
+++ src/screens/session-browser.screen.tsx
@@ -1,22 +1,27 @@
 import * as React from 'react'
 import 'firebase/functions'
-import { useFunctions } from 'reactfire'
+import 'firebase/firestore'
+import { useFunctions, useUser, useFirestoreCollectionData, useFirestore } from 'reactfire'
 
 import { Input, Button } from '@chakra-ui/react'
 
 const SessionBrowser: React.FC = () => {
   const [email, setEmail] = React.useState<string>('')
 
+  const user = useUser()
+  const sessionsQuery = useFirestore().collection('sessions').where('invitations', 'array-contains', user.data.email)
+  const sessions = useFirestoreCollectionData(sessionsQuery)
+  console.log(sessions)
+
   const functions = useFunctions()
   const callable = functions.httpsCallable('emailInvitation')
-
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setEmail(event.target.value)
-  }
-
   function sendEmail() {
     console.log('hi')
     callable({ email }).then(console.log).catch(console.log)
+  }
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setEmail(event.target.value)
   }
 
   return (
